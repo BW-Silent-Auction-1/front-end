1c904e90015c562cc8632cbd0026cb3a66e2b248
Cobrettie <cobrettiegarner3@gmail.com>
2020-05-22 06:37:55 -0400

    wrap App in Provider

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const store = configureStore({
 })
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
