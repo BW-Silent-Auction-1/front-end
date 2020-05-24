bb2abf838059022f82173b9336122e764137295b
Cobrettie <cobrettiegarner3@gmail.com>
2020-05-24 19:44:04 -0400

    update App.js to match master App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import ls from './components/Login&SignUp';
+import FadeIn from 'react-fade-in';
 import AddAuctionItem from './Redux-Auth/AddAuctionItem';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <AddAuctionItem />
-    </div>
-  );
-}
+const App = () => (
+  <div className='App'>
+
+    <FadeIn>
+      <Header/>
+    </FadeIn>
+
+    <Route exact path="/" component={Home}/>
+    <Route path="/Auction"/>
+    <Route path="/Login/SignUp" component={ls}/>
+    
+    <FadeIn>
+      <Footer/>
+    </FadeIn>
+
+  </div>
+
+);
+
 
 export default App;
