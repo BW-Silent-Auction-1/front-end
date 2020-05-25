83eb4268d026664c43e28131366d8396526b7218
Cobrettie <cobrettiegarner3@gmail.com>
2020-05-24 22:10:00 -0400

    add Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {PageHeader} from 'antd';
+import Navigation from './Navigation'
+
+const Header = () => {
+  return(
+      <PageHeader style={{ width:'100%', color:'white', fontFamily: 'DM Mono, monospace', backgroundColor:'rgb(53, 53, 53)', border:'1px solid black'}}
+        className="site-page-header"
+        onBack={() => null}
+        type='ghost'
+        title="Silent Auction"
+        subTitle="Buy and sell, anonymously!"
+        ghost='true'
+        FontColor='White'
+      >
+
+    <Navigation />
+    </PageHeader>
+  )
+}
+
+export default Header;
