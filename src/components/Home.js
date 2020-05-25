2c1b1672e8a4f4acc28621bd437c214a76102caa
Cobrettie <cobrettiegarner3@gmail.com>
2020-05-24 22:11:19 -0400

    add Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FadeIn from 'react-fade-in';
+
+const Home = () => {
+  return(
+    <FadeIn transitionDuration='5000' delay='500'>
+      <div className='homeInfo'> 
+        <h1 className="headers">Welcome to Silent-Auction!</h1>
+        <p>We're pleased to welcome you aboard to our online auction platform,
+        were buyers and sellers can do so while being in the comfort of thier own home (anonymously!). </p>
+      </div>
+    </FadeIn>
+)}
+
+export default Home;
